6e27c02bc1ebe5d28a740a7b80483d33c46477c8
Dries De Smet <driesdesmet@gmail.com>
2023-12-02 15:14:16 +0100

    Refactor 2023 02 to structs

--- src/bin/2023_02.rs
+++ src/bin/2023_02.rs
@@ -1,4 +1,4 @@
-    use std::env;
+use std::{cmp, env};
 use std::fs::read_to_string;
 
 fn main() {
@@ -6,101 +6,103 @@ fn main() {
 
     let file_path = &args[1];
 
-    let input =  read_to_string(file_path).unwrap();
+    let input = read_to_string(file_path).unwrap();
 
-    let total = part_2(input);
+    let total = parse_games(input);
 
+    let max_cubes = Cubes {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };  
 
-    println!("{}", total);
+    let total_part_1 = total.iter()
+        .filter(|game| game.max.blue <= max_cubes.blue && game.max.green <= max_cubes.green && game.max.red <= max_cubes.red)
+        .map(|game| game.id)
+        .sum::<i32>();
 
+    let total_part_2 = total.iter()
+        .map(|game| game.max.green * game.max.red * game.max.blue)
+        .sum::<i32>();
+
+    println!("Part 1: {}", total_part_1);
+    println!("Part 2: {}", total_part_2);
+}
+
+struct Game {
+    id: i32,
+    moves: Vec<Cubes>,
+    max: Cubes,
+}
+
+impl Game {
+
+    fn new(id: i32) -> Self {
+        Self {
+            id,
+            moves: Vec::new(),
+            max: Cubes::new()
+        }
+    }
+
+    fn add_move(&mut self, cube_move: Cubes) {
+        self.max.red = cmp::max(self.max.red, cube_move.red);
+        self.max.green = cmp::max(self.max.green, cube_move.green);
+        self.max.blue = cmp::max(self.max.blue, cube_move.blue);
+
+        self.moves.push(cube_move);
+    }
+}
+
+struct Cubes {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+impl Cubes {
+    fn new() -> Self {
+        Self {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+
+    fn set_colour(&mut self, colour: &str, count: i32) {
+        match colour {
+            "blue" => self.blue += count,
+            "red" => self.red += count,
+            "green" => self.green += count,
+            _ => {}
+        }
+    }
 }
 
 
-fn part_1(input: String) -> i32 {
-    input.split("\n").fold(0, |sum, game_line| {
+fn parse_games(input: String) -> Vec<Game> {
+    input.split("\n").fold(Vec::new(), |mut games, game_line| {
         let (game, game_info): (&str, &str) = game_line.split_once(":").unwrap();
 
-        let game_id: i32 = game.split_once(" ").unwrap().1.parse::<i32>().unwrap();
+        let (_, game_id) = game.split_once(" ").unwrap();
 
-        let mut max_blue = 0;
-        let mut max_green = 0;
-        let mut max_red = 0;
+        let mut game = Game::new(game_id.parse().unwrap());
 
         for single_game in game_info.split(";") {
             let draws = single_game.split(',');
 
-            for draw in draws {
-                let (count_str, colour) = draw.trim().split_once(" ").unwrap();
-
-                let count = count_str.parse::<i32>().unwrap();
-
-                match colour {
-                    "blue" => {
-                        if count > max_blue {
-                            max_blue = count;
-                        }
-                    },
-                    "red" => {
-                        if count > max_red {
-                            max_red = count;
-                        }
-                    }
-                    "green" => {
-                        if count > max_green {
-                            max_green = count;
-                        }
-                    },
-                    _ => {}
-                }
-            }
-        }
-
-        if max_red <= 12 && max_green <= 13 && max_blue <= 14 {
-            sum + game_id
-        } else {
-            sum
-        }
-    })
-}
-fn part_2(input: String) -> i32 {
-    input.split("\n").fold(0, |sum, game_line| {
-        let (game, game_info): (&str, &str) = game_line.split_once(":").unwrap();
-
-        let game_id: i32 = game.split_once(" ").unwrap().1.parse::<i32>().unwrap();
-
-        let mut max_blue = 0;
-        let mut max_green = 0;
-        let mut max_red = 0;
-
-        for single_game in game_info.split(";") {
-            let draws = single_game.split(',');
+            let mut cubes_for_draw = Cubes::new();
 
             for draw in draws {
                 let (count_str, colour) = draw.trim().split_once(" ").unwrap();
-
                 let count = count_str.parse::<i32>().unwrap();
 
-                match colour {
-                    "blue" => {
-                        if count > max_blue {
-                            max_blue = count;
-                        }
-                    },
-                    "red" => {
-                        if count > max_red {
-                            max_red = count;
-                        }
-                    }
-                    "green" => {
-                        if count > max_green {
-                            max_green = count;
-                        }
-                    },
-                    _ => {}
-                }
+                cubes_for_draw.set_colour(colour, count);
             }
+            game.add_move(cubes_for_draw);
         }
 
-            sum + (max_red * max_blue * max_green)
+        games.push(game);
+        games
     })
 }
